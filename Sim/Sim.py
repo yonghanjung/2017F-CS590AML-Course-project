5f6b237a38374c062ec448ea2426657334c75982
Hans Jung <yhansjung@gmail.com>
2018-01-19 17:02:51 -0500

    170119

--- Sim/Sim.py
+++ Sim/Sim.py
@@ -6,19 +6,55 @@ from sklearn import mixture
 from scipy.stats import gaussian_kde
 from scipy.optimize import minimize
 
+class MyError(Exception):
+    pass
+
 class DataGen(object):
+    '''
+    Class DataGen contains all data and methods related to
+    generating data.
+    - Input: D (dim), N (num_obs), Ns (num_sample_intv), Mode (easy, crazy)
+    - Output: Observational data and Interventional data
+    '''
+
+    # Seed fix
+    # Some good seed are 1234, 12345.
     np.random.seed(12345)
     def __init__(self,D,N,Ns,Mode):
+        '''
+        Initializing the class
+        :param D: Dimension
+        :param N: Number of observations and interventions (true)
+        :param Ns: Number of sampled observation, Ns << N.
+        :param Mode:
+            - Easy: easy parametrization (Normal), assuming SCM is known
+            - Crazy: crazy parametrization, arbitrary density
+        '''
+
+        '''
+        Declaring input as global in the class
+        to contain the input value as data. 
+        '''
         self.dim = D
         self.num_obs = N
         self.num_intv = Ns
         self.Mode = Mode # easy / crazy
 
     def intfy(self,W):
+        '''
+        Transform the real-number array (e.g., 1.0,2.0) to 
+        the integer array (e.g., 1, 2).  
+        :param W: Real-number array 
+        :return: Integer array
+        '''
         return np.array(list(map(int, W)))
 
     def weird_projection(self, X):
-        # X can be either matrix or vector
+        '''
+        Project
+        :param X:
+        :return:
+        '''
         X_proj = np.log(np.abs(X)+20) * np.arctan(X) + (10*np.sin(X)) - (np.tan(X ** 2) + 20) * np.exp(X-20)
         X_proj = (X_proj - np.mean(X_proj, axis=0)) / np.var(X_proj)
         return X_proj
@@ -159,21 +195,38 @@ class DataGen(object):
         self.gen_Y()
         self.structure_data()
 
-
-
 class DP_sim(DataGen):
     def __init__(self,D,N,Ns,Mode,x):
-        self.dim = D
-        self.num_obs = N
-        self.num_intv = Ns
-        self.Mode = Mode # easy / crazy
         self.x = x
+        super().__init__(D,N,Ns,Mode)
 
         self.data_gen()
         self.Y_x = self.Obs[self.Obs['X']==self.x]['Y']
         self.Yinv_x = self.Intv[self.Intv['X']==self.x]['Y']
         self.Ysinv_x = self.Intv_S[self.Intv_S['X']==self.x]['Y']
 
+    def preprocess(self, X):
+        if X.shape[1] >= X.shape[0] or pd.isnull(X.shape[1]):
+            X = np.matrix(X).T
+        else:
+            pass
+        return X
+
+    def Fit(self, X):
+        X = np.reshape(X, (len(X), 1))
+        X = self.preprocess(X)
+        init_compo = 50
+        dpgmm= mixture.BayesianGaussianMixture(
+            n_components=init_compo, weight_concentration_prior=1 / self.num_obs,
+            max_iter=1000, tol=1e-10, ).fit(X)
+        return dpgmm
+
+    def DP_fit(self):
+        self.dpobs = self.Fit(self.Y_x)
+        self.dpintv = self.Fit(self.Y_intv)
+        self.dpintv_s = self.Fit(self.Ysinv_x)
+
+class Graph(DP_sim):
     def Graph_obs_int(self):
         f = plt.figure()
 
@@ -194,32 +247,13 @@ class DP_sim(DataGen):
         x_domain_int = x_domain_obs
         int_plot.plot(x_domain_int, intv_density(x_domain_int))
         int_plot.hist(X_int, 100, normed=True)
-
         return f
 
-    def preprocess(self, X):
-        if X.shape[1] >= X.shape[0] or pd.isnull(X.shape[1]):
-            X = np.matrix(X).T
-        else:
-            pass
-        return X
-
-    def Fit(self, X):
-        X = np.reshape(X, (len(X), 1))
-        X = self.preprocess(X)
-        init_compo = 20
-        dpgmm= mixture.BayesianGaussianMixture(
-            n_components=init_compo, weight_concentration_prior=1 / self.num_obs,
-            max_iter=1000, tol=1e-10, ).fit(X)
-        return dpgmm
-
-    def DP_fit(self):
-        self.dpobs = self.Fit(self.Y_x)
-        self.dpintv = self.Fit(self.Y_intv)
-        self.dpintv_s = self.Fit(self.Ysinv_x)
-
-
     def Graph_DPFit(self):
+        if self.Mode == 'easy':
+            print("Graph_DPFit method is only for 'crazy' mode")
+            raise MyError
+
         f = plt.figure()
 
         X_int = np.reshape(self.Yinv_x, (len(self.Yinv_x), 1))
@@ -239,12 +273,10 @@ class DP_sim(DataGen):
         sim_density = gaussian_kde(X_sim)
         # sim_x_domain = np.linspace(min(X_sim) - 5, max(X_sim) + 5, self.num_data)
         sim_plot.plot(x_domain_int, sim_density(x_domain_int))
-        sim_plot.hist(X_sim, 100, normed=True)
-
+        sim_plot.hist(X_sim, self.num_obs/100, normed=True)
         return f
 
-
-class KL_computation(DP_sim):
+class KL_computation():
     def KL_Gaussian(self, f_mean, f_std, g_mean, g_std):  # KL(f || g)
         return np.log(g_std / f_std) + \
                (f_std ** 2 + (f_mean - g_mean) ** 2) / (2 * g_std ** 2) - \
@@ -285,24 +317,8 @@ class KL_computation(DP_sim):
             sum_result += pi_k * np.log( (sum_numer + reducing)  / (sum_deno + reducing))
         return sum_result
 
-    def KL(self):
-        if self.Mode == 'easy':
-            f_mean = np.mean(self.Y_x)
-            g_mean = np.mean(self.Yinv_x)
-            f_std = np.std(self.Y_x)
-            g_std = np.std(self.Yinv_x)
-            return self.KL_Gaussian(f_mean,f_std,g_mean,g_std)
-        elif self.Mode == 'crazy':
-            self.DP_fit()
-            f_means = np.round(self.dpobs.means_, 3)
-            g_means = np.round(self.dpintv.means_, 3)
-            f_weights = np.round(self.dpobs.weights_,3)
-            g_weights = np.round(self.dpintv.weights_, 3)
-            f_stds = np.ndarray.flatten( np.round( np.sqrt(self.dpobs.dpgmm.covariances_),3 ) )
-            g_stds = np.ndarray.flatten( np.round( np.sqrt(self.dpintv.dpgmm.covariances_),3 ) )
-            return self.KL_GMM(f_weights, g_weights, f_means, g_means, f_stds, g_stds)
 
-class CausalBound(KL_computation):
+class CausalBound(DP_sim, KL_computation):
     def Set_x(self, x):
         self.x = x
         self.Y_x = self.Obs[self.Obs['X'] == self.x]['Y']
@@ -310,8 +326,8 @@ class CausalBound(KL_computation):
         self.Ysinv_x = self.Intv_S[self.Intv_S['X'] == self.x]['Y']
         if self.Mode == 'crazy':
             self.DP_fit()
-        self.KL()
-        self.ComputeBound()
+        # self.KL()
+        # self.ComputeBound()
 
     def Entropy_x(self):
         px = len(self.Obs[ self.Obs['X']==self.x ])/self.num_obs
@@ -321,7 +337,7 @@ class CausalBound(KL_computation):
     def ComputeBound(self):
         minus_logpx = self.Entropy_x()
         if self.Mode == 'easy':
-            true_mu_do = np.mean(self.Y_intv)
+            true_mu_do = np.mean(self.Yinv_x)
             mu_obs = np.mean(self.Y_x)
             std_obs = np.std(self.Y_x)
             std_do = np.std(self.Ysinv_x)
@@ -334,9 +350,9 @@ class CausalBound(KL_computation):
         elif self.Mode == 'crazy':
             self.DP_fit()
             Hx = self.Entropy_x()
-            rounding_digit = 3
+            rounding_digit = 4
             f_means = np.round(self.dpobs.means_, rounding_digit)
-            # g_means = np.round(self.dpintv.means_, rounding_digit)
+            true_means = np.round(self.dpintv.means_, rounding_digit)
             f_weights = np.round(self.dpobs.weights_, rounding_digit)
             g_weights = np.round(self.dpintv_s.weights_, rounding_digit)
             f_stds = np.ndarray.flatten(np.round(np.sqrt(self.dpobs.covariances_), rounding_digit))
@@ -353,13 +369,122 @@ class CausalBound(KL_computation):
             min_fun = lambda mu_do: np.sum(mu_do * g_weights)
             max_fun = lambda mu_do: -np.sum(mu_do * g_weights)
 
-            lower = minimize(min_fun, x0=x0, constraints=cons, method='SLSQP',options={'maxiter':100,'disp':True})
-            upper = minimize(max_fun, x0=x0, constraints=cons, method='SLSQP',options={'maxiter':100,'disp':True})
-            return [np.sum(lower.x * g_weights),np.sum(x0 * g_weights),np.sum(upper.x*g_weights)], lower, upper
+            max_iter = 1000
+            lower = minimize(min_fun, x0=x0, constraints=cons, method='SLSQP',options={'maxiter':max_iter,'disp':True})
+            upper = minimize(max_fun, x0=x0, constraints=cons, method='SLSQP',options={'maxiter':max_iter,'disp':True})
+            # return [np.sum(lower.x * g_weights),np.sum(true_means * g_weights),np.sum(upper.x*g_weights)], lower, upper
+            return [np.sum(lower.x * g_weights), np.sum(true_means * g_weights),
+                    np.sum(upper.x * g_weights)]
 
-cb = CausalBound(10,1000,500,'crazy',0)
-result, lower, upper = cb.ComputeBound()
+class B_KLUCB(CausalBound):
+    '''
+    Currently this class is only applicable for 'easy' mode
+    '''
+    def __init__(self, D,N,Ns,Mode,T, K):
+        super().__init__(D, N, Ns, Mode, x)
+        if self.Mode == 'crazy':
+            print("Crazy mode is not allowed")
+            raise MyError()
+        self.T = T # number of iteration of bandits
+        self.K = K # number of arms
 
+    def CutArm(self):
+        # arm collection
+        ## arm collection for arm 0
+        self.Set_x(x=0)
+        bd_0 = self.ComputeBound()
+        lb0 = bd_0[0]
+        ub0 = bd_0[2]
+        arm_0 = [lb0, ub0]
+
+        ## arm collection for arm 1
+        self.Set_x(x=1)
+        bd_1 = self.ComputeBound()
+        lb1 = bd_1[0]
+        ub1 = bd_1[2]
+        arm_1 = [lb1, ub1]
+        arms = [arm_0, arm_1]
+
+        ## arm collection for lb and ub for each arms.
+        l_arm = [lb0, lb1]
+        u_arm = [ub0, ub1]
+        l_max = np.max(l_arm)
+
+        # Cutting arm
+        arms_new = dict()
+        l_arm_new = dict()
+        u_arm_new = dict()
+        arms_idx = list()
+
+        for a in range(K):
+            # cur arm condition
+            if u_arm[a] < l_max:
+                continue
+            else:
+                arms_new[a] = arms[a]
+                l_arm_new[a] = l_arm[a]
+                u_arm_new[a] = u_arm[a]
+                arms_idx.append(a)
+
+        # Declare remaing arms
+        self.arms = arms_new
+        self.l_arm = l_arm_new
+        self.u_arm = u_arm_new
+        self.armidx = arms_idx
+        self.K = len(self.armidx)
+
+    def Receive_rewards(self,arm,num_armpull):
+        pulling_idx = num_armpull[arm]-1
+        return list(self.Intv[self.Intv['X']==arm]['Y'])[pulling_idx]
+
+
+    def Bandit_Init(self):
+        '''
+        Pulling each arm one time.
+        :return:
+        '''
+
+        Arm = []
+        Reward = []
+
+        num_armpull = dict()
+        reward_armpull = dict()
+        for a in self.armidx:
+            # initial arm pull
+            num_armpull[a] = 1
+            reward_armpull[a] = [self.Receive_rewards(a,num_armpull)]
+        
+
+
+
+
+    def Bandit(self):
+        self.CutArm()
+        if len(self.armidx) == 1:
+            return self.armidx
+
+        else:
+            self.Bandit_Init()
+
+
+
+
+
+
+
+#################### MAIN ############################
+D = 3
+N = 1000
+Ns = 500
+Mode = 'easy'
+x = 0
+T = 1000
+K = 2
+bkl = B_KLUCB(D,N,Ns,Mode,T,K)
+graph = Graph(D,N,Ns,Mode,x)
+# result, lower, upper = cb.ComputeBound()
+# cb.Graph_obs_int()
+# cb.Graph_DPFit()
 
 
 
